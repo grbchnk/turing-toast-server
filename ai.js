b01ff0665cc807c661850c26da96dd3572c7e955
grbchnk <stsrnbw@gmail.com>
2025-12-15 06:03:53 +0300

    1.5

--- ai.js
+++ ai.js
@@ -4,7 +4,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Инициализация
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 async function generateAiAnswer(question, playerAnswers) {
   try {
